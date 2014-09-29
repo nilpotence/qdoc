bdded5a77eaa908844422a336a42749d9f68cd65
Sylvain Mahé <sylv.mahe@gmail.com>
2014-09-29 15:31:43 +0200

    add title anchor support

--- src/converters/markdown.js
+++ src/converters/markdown.js
@@ -1,16 +1,32 @@
 var marked = require('marked');
 var fs = require('fs.extra');
 
+
+var renderer = new marked.Renderer();
+
+renderer.heading = function (text, level) {
+	  var escapedText = text.toLowerCase().replace(/[^\w]+/g, '-');
+
+	  return '<h' + level + '><a name="' +
+	                escapedText +
+	                 '" class="anchor" href="#' +
+	                 escapedText +
+	                 '"><span class="header-link"></span></a>' +
+	                  text + '</h' + level + '>';
+	};
+
+var marked = require('marked');
 marked.setOptions({
-	renderer: new marked.Renderer(),
-	gfm: true,
-	tables: true,
-	breaks: false,
-	pedantic: false,
-	sanitize: true,
-	smartLists: true,
-	smartypants: true
+  renderer: renderer,
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false
 });
+
 
 function Markdown(){
 	
